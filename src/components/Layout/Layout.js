1b9dd02d7af072a125084612c289ec215156e711
Nickolas Fuentes <nickfuentes24@gmail.com>
2021-04-07 21:49:27 -0500

    Created a layout css module to style the main element inside layouts component. Also updated the version of react scripts to use the modules feature.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from "react"
 
 import Aux from "../../hoc/Aux"
+import classes from "./Layout.module.css"
 
 const layout = (props) => (
   <Aux>
     <div>Toolbar, Sidedrawer, Backdrop</div>
-    <main>{props.children}</main>
+    <main className={classes.content}>{props.children}</main>
   </Aux>
 )
 
