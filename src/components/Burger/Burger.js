cf71e60e1ab67feabc64a08faae072041f2f83fa
Nickolas Fuentes <nickfuentes24@gmail.com>
2021-04-09 23:44:21 -0500

    Created static state in the Burger Builder and passed it down to the Burger component. Burger js is an object state that is converted to an array and gets mapped to show ingredients in view.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,15 @@ import classes from "./Burger.module.css"
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient"
 
 const burger = (props) => {
+  const transformedIngredients = Object.keys(props.ingredients).map((igKey) => {
+    return [...Array(props.ingredients[igKey])].map((_, i) => {
+      return <BurgerIngredient key={igKey + i} type={igKey} />
+    })
+  })
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top"></BurgerIngredient>
-      <BurgerIngredient type="cheese"></BurgerIngredient>
-      <BurgerIngredient type="meat"></BurgerIngredient>
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom"></BurgerIngredient>
     </div>
   )
