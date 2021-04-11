ce786af0d75f574c549c9a455f0df4a937b8016d
Nickolas Fuentes <nickfuentes24@gmail.com>
2021-04-11 15:10:34 -0500

    Created the build controls and build control components as well as the css files for styling. Just assigning styleing to the components.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,18 +5,18 @@ import Burger from "../../components/Burger/Burger"
 
 class BurgerBuilder extends Component {
   state = {
-      ingredients: {
-          salad: 0,
-          bacon: 0,
-          cheese: 0,
-          meat: 0
-      }
+    ingredients: {
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0,
+    },
   }
 
   render() {
     return (
       <Aux>
-        <Burger ingredients={this.state.ingredients}/>
+        <Burger ingredients={this.state.ingredients} />
         <div>Build Controls</div>
       </Aux>
     )
