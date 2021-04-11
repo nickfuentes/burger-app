0b1db3141f49d7b79b27affc98c0c95e0f23bce2
Nickolas Fuentes <nickfuentes24@gmail.com>
2021-04-11 15:53:52 -0500

    Had to rename some of the css module folders from modules to module. Moved the buildcontrols to render in the burger builder component. Now all the styling for the controls are showing in the view.

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-import classes from "./buildControl.module.css"
+import classes from "./BuildControl.module.css"
 
 const buildControl = (props) => (
   <div className={classes.BuildControl}>
