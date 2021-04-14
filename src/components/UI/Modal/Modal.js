27776074fa27256e2b06c435d0f1786c6177b97c
Nickolas Fuentes <nickfuentes24@gmail.com>
2021-04-14 18:23:34 -0500

    Create a purchasing handler and another state option. Depening on the new purchasing state option we pass it down to the modal and use a ternary operator to give default styling or change it if state changes so css can show.

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react"
 
-import classes from './Modal.module.css'
+import classes from "./Modal.module.css"
 
 const modal = (props) => (
-    <div className={classes.Modal}>
-        {props.children}
-    </div>
+  <div
+    style={{ transform: props.show ? "translateY(0)" : "translateY(-100vh)", opacaity : props.show ? '1' : '0' }}
+    className={classes.Modal}
+  >
+    {props.children}
+  </div>
 )
 
 export default modal
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -23,6 +23,7 @@ class BurgerBuilder extends Component {
     },
     totalPrice: 4,
     purchasable: false,
+    purchasing: false,
   }
 
   updatePurchaseState(ingredients) {
@@ -67,6 +68,10 @@ class BurgerBuilder extends Component {
     this.updatePurchaseState(updatedIngredients)
   }
 
+  purchaseHandler = () => {
+    this.setState({ purchasing: true })
+  }
+
   render() {
     const disabledInfo = {
       ...this.state.ingredients,
@@ -76,7 +81,7 @@ class BurgerBuilder extends Component {
     }
     return (
       <Aux>
-        <Modal>
+        <Modal show={this.state.purchasing}>
           <OrderSummary ingredients={this.state.ingredients} />
         </Modal>
         <Burger ingredients={this.state.ingredients} />
@@ -86,6 +91,7 @@ class BurgerBuilder extends Component {
           disabled={disabledInfo}
           purchasable={this.state.purchasable}
           price={this.state.totalPrice}
+          ordered={this.purchaseHandler}
         />
       </Aux>
     )
