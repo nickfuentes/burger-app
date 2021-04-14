cb6be7df5b27b25ee83df8a905c48e82d4b8dfdb
Nickolas Fuentes <nickfuentes24@gmail.com>
2021-04-14 18:56:19 -0500

    Created an onClick handler to for the backdrop fade to be clickable when a user wants to cancel/exit out of the purchasing modal.

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,14 +1,22 @@
 import React from "react"
 
 import classes from "./Modal.module.css"
+import Aux from "../../../hoc/Aux"
+import Backdrop from "../Backdrop/Backdrop"
 
 const modal = (props) => (
-  <div
-    style={{ transform: props.show ? "translateY(0)" : "translateY(-100vh)", opacaity : props.show ? '1' : '0' }}
-    className={classes.Modal}
-  >
-    {props.children}
-  </div>
+  <Aux>
+    <Backdrop show={props.show} clicked={props.modalClosed} />
+    <div
+      style={{
+        transform: props.show ? "translateY(0)" : "translateY(-100vh)",
+        opacaity: props.show ? "1" : "0",
+      }}
+      className={classes.Modal}
+    >
+      {props.children}
+    </div>
+  </Aux>
 )
 
 export default modal
